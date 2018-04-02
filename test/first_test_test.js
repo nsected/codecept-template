218c58d2c6b2c644bf8447867c4e20f677a72763
nsected@gmail.com <nsected@gmail.com>
2018-04-02 20:05:29 +0500

    tst

--- test/first_test_test.js
+++ test/first_test_test.js
@@ -2,6 +2,9 @@
 Feature('kabinet');
 
 Scenario('test something', (I) => {
+    console.log(1);
     I.amOnPage('/helpers/WebDriverIO/');
+    console.log(2);
     I.see('Почта');
+    console.log(3);
 });

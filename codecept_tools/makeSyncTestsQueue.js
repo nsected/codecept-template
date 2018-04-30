ca16b2a9fba538b8c1b5f4644caeb3201a696c37
mustaev <mustaev123456>
2018-04-30 21:25:42 +0500

    split with modules

--- codecept_tools/makeSyncTestsQueue.js
+++
@@ -1,9 +0,0 @@
-module.exports = function makeSyncTestsQueue(configPath, overrideArguments, config) {
-    return [{
-        name: config.name,
-        status: 'waiting',
-        overrideArguments: overrideArguments,
-        configPath: configPath,
-        specificTestFile: false
-    }]
-}

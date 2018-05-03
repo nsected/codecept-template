db3df63f383554084788e51480b56215a3fd1d12
d.mustaev <d.mustaev@xsolla.com>
2018-05-03 22:08:50 +0500

    §

--- tests/publisher3/login_prod.js
+++ tests/publisher3/login_prod.js
@@ -1,8 +1,8 @@
-module.exports = async function login(I) {
+module.exports = async function login(I, vars) {
     I.amOnPage('/signin');
     await I.clickOn('//button[@type="submit"]');
-    await I.fillField('#email', 'kochetovatest+40@gmail.com');
-    await I.fillField('#password', 'U6GRTnlxz');
+    await I.fillField('#email', vars.email);
+    await I.fillField('#password', vars.password);
     await I.clickOn('//button[@type="submit"]');
     await I.waitForElement('.username');
 };
--- tests/publisher3/publisher_smart_login.js
+++ tests/publisher3/publisher_smart_login.js
@@ -2,6 +2,6 @@ module.exports = async function login(I, login, vars) {
     if (vars.type === 'demo') {
         await require('./login_demo.js')(I);
     } else {
-        await login.login(I);
+        await login.login(I, vars);
     }
 };
